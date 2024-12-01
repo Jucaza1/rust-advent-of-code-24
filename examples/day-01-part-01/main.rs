30817e65f505900493079881e51b765374b1b15d
jucaza1 <juan01cz@gmail.com>
2024-12-01 23:43:01 +0100

    added day 1 part 1 and 2

--- examples/day-01-part-01/main.rs
+++ examples/day-01-part-01/main.rs
@@ -5,18 +5,31 @@ use std::{
 };
 
 fn main() {
-    let file = File::open("./examples/day-01-part-01/input-test.txt").expect("file not found");
-    // let file = File::open("./examples/day-01-part-01/input.txt").expect("file not found");
+    // let file = File::open("./examples/day-01-part-01/input-test.txt").expect("file not found");
+    let file = File::open("./examples/day-01-part-01/input.txt").expect("file not found");
     let buff = BufReader::new(file);
     let mut acc = 0;
+    let (mut left_v, mut right_v): (Vec<i32>, Vec<i32>) = (Vec::new(), Vec::new());
     for line in buff.lines() {
-        let Ok(line) = line else{
+        let Ok(line) = line else {
             panic!("line not present")
         };
-        acc += process_line(&line)
+        let mut split_line = line.split("   ");
+        let (Some(left_n), Some(right_n), None) = (
+            split_line.next().and_then(|x| x.parse::<i32>().ok()),
+            split_line.next().and_then(|x| x.parse::<i32>().ok()),
+            split_line.next(),
+        ) else {
+            panic!("expected 2 numbers separated with 3 spaces")
+        };
+        left_v.push(left_n);
+        right_v.push(right_n);
     }
-    println!("Result -> {}",acc);
-}
-fn process_line(s: &str) -> i32 {
-    0
+    left_v.sort();
+    right_v.sort();
+    for i in 0..left_v.len() {
+        acc += (left_v.get(i).expect("mismatch length") - right_v.get(i).expect("mismatch length"))
+            .abs();
+    }
+    println!("Result -> {}", acc);
 }
--- examples/day-01-part-02/main.rs
+++ examples/day-01-part-02/main.rs
@@ -1,22 +1,43 @@
 use core::panic;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
 fn main() {
-    let file = File::open("./examples/day-01-part-02/input-test.txt").expect("file not found");
-    // let file = File::open("./examples/day-01-part-02/input.txt").expect("file not found");
+    // let file = File::open("./examples/day-01-part-01/input-test.txt").expect("file not found");
+    let file = File::open("./examples/day-01-part-01/input.txt").expect("file not found");
     let buff = BufReader::new(file);
     let mut acc = 0;
+    let (mut left_v, mut right_v): (Vec<i32>, Vec<i32>) = (Vec::new(), Vec::new());
     for line in buff.lines() {
-        let Ok(line) = line else{
+        let Ok(line) = line else {
             panic!("line not present")
         };
-        acc += process_line(&line)
+        let mut split_line = line.split("   ");
+        let (Some(left_n), Some(right_n), None) = (
+            split_line.next().and_then(|x| x.parse::<i32>().ok()),
+            split_line.next().and_then(|x| x.parse::<i32>().ok()),
+            split_line.next(),
+        ) else {
+            panic!("expected 2 numbers separated with 3 spaces")
+        };
+        left_v.push(left_n);
+        right_v.push(right_n);
     }
-    println!("Result -> {}",acc);
-}
-fn process_line(s: &str) -> i32 {
-    0
+    left_v.sort();
+    right_v.sort();
+    let mut n_map: HashMap<i32, i32> = HashMap::new();
+    for n in left_v.into_iter() {
+        acc += n * *n_map.entry(n).or_insert(
+            right_v
+                .iter()
+                .filter(|x| **x == n)
+                .count()
+                .try_into()
+                .unwrap(),
+        );
+    }
+    println!("Result -> {}", acc);
 }
